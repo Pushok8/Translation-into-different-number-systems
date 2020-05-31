3aada2c677b53caae2d181e9a9adb884d44c69a0
Pushok8 <60236064+Pushok8@users.noreply.github.com>
2020-05-31 20:23:18 +0300

    number_system.py
    Added annotations, competent documentation, removed unnecessary comments by type: "x = x + 1  # Increases x by one.", Adapted to PEP8 rules. I will continue to continue the changes during my training (if this is of course required).

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
-# Creator: ©Pushok8.
-# Date: 31 March 2020. COVID-19, hello!)
-# Edit: 1 April 2020.
-# Has changed: add documentation, changed the location of functions.
-# Edit: 23 April 2020.
-# The function of converting to a convenient format has been removed and replaced with a more correct one.
+"""
+The number_system module: translates the resulting number to the desired encoding.
 
-NUMBER_SYSTEM = [
+Module contains one function that translates the specified number by the user from
+ the native encoding to the desired final encoding.
+"""
+
+from typing import TypeVar
+
+__all__ = ['number_encoding']
+__author = '©Pushok8'
+int_str = TypeVar('number', str, int)
+_NUMBER_SYSTEM = [
     0, 1, 2, 3, 4, 5,
     6, 7, 8, 9, 'A', 'B',
     'C', 'D', 'E', 'F', 'G', 'H',
@@ -14,16 +19,12 @@ NUMBER_SYSTEM = [
     'U', 'V', 'W', 'X', 'Y', 'Z'
 ]
 
-def get_decimal_numsys_method(number, number_system):  # Decimal method.
-    # The method of conversion to the decimal number system is the conversion
-    # from any number system to decimal by multiplying the numbers from the end
-    # of the number system which is indicated to the power starting from zero.
 
+def _get_decimal(number: int, number_system: int) -> int:
     convenient_encoding_form = str(number)[::-1]
-    i = 0
-    decimal_encoding = 0
+    i: int = 0
+    decimal_encoding: int = 0
 
-    # Translates from system less than 10 to decimal system.
     for digit in convenient_encoding_form:
         if digit.isdigit():
             decimal_encoding += NUMBER_SYSTEM.index(int(digit)) * number_system ** i
@@ -31,15 +32,12 @@ def get_decimal_numsys_method(number, number_system):  # Decimal method.
             decimal_encoding += NUMBER_SYSTEM.index(digit) * number_system ** i
         i += 1
 
-    return decimal_encoding  # Return number in decimal.
+    return decimal_encoding
 
-def get_binary_numsys_method(number, number_system):  # Binary numeration method.
-    # The binary number system is a way to get the binary number system
-    # from the decimal system of the number system by dividing by 2 or
-    # the number system that was indicated.
 
-    response = []  # The answer will gradually be added here.
-    cache = [int(number)]  # Create cache as RAM which will soon be cleansed.
+def _get_binary(number: int, number_system: int) -> int:
+    response = []
+    cache = [int(number)]
     b = 0
 
     while cache[b] > 0:
@@ -54,46 +52,40 @@ def get_binary_numsys_method(number, number_system):  # Binary numeration method
 
     response.reverse()
     response = ''.join(response)
-    del cache
 
     if response.isalnum():
         return response
 
-    return int(response)  # Return number in binary or other number system.
-
-def translateToNumSys(number, initial_number_system=10, final_number_system=10):
-    '''
-    translateToNumSys(number, initial_number_system=10, final_number_system=10)
-    Converts from one number system to another. The maximum number system is 36.
-    :param number:                number in the number system you specify.
-    :param initial_number_system: initial number system of the number. The default is 10.
-    :param final_number_system:   finite number system. The default is 10.
-    :return:                      integer number converted to the specified number system.
-    '''
+    return int(response)
 
 
-    # Сhecks to write the number in the correct encoding.
+def number_encoding(number: int_str = 0, initial_number_system: int = 10, final_number_system: int = 10) -> int_str:
+    """
+    number_encoding(number=10, initial_number_system=10, final_number_system=10)
+
+    It transfers from the received data on the number and its number system and returns to another specified number
+     system. The maximum number system is 36.
+    :param number: number in the number system you specify. The default is 10.
+    :param initial_number_system: Native (in which the number is located) number system. The default is 10.
+    :param final_number_system: The final (into which the number will be converted) number system. The default is 10.
+    :return: number converted from the 'initial_number_system' number system to the 'final_number_system' number system.
+    """
     for i in list(str(number)):
         if str(i).isdigit():
             assert NUMBER_SYSTEM.index(
                 int(i)) < initial_number_system, 'You entered the number or symbol in the wrong encoding!'
         else:
-            assert NUMBER_SYSTEM.index(i) < initial_number_system, 'You entered the number or symbol in the wrong encoding!'
+            assert NUMBER_SYSTEM.index(
+                i) < initial_number_system, 'You entered the number or symbol in the wrong encoding!'
 
     assert final_number_system <= 36, 'Maximum transfer from number system to another number system - 36!'
     assert initial_number_system <= 36, 'Maximum transfer from number system to another number system - 36!'
 
-    # Just returns what they gave.
     if final_number_system == initial_number_system:
         return number
-
-
-    if initial_number_system != 10 and final_number_system == 10:  # Translate to decimal number system.
-        return get_decimal_numsys_method(number, initial_number_system)
-
-    elif initial_number_system != 10 and final_number_system != 10:  # Translate to other number system.
-        decimalEncodingValues = get_decimal_numsys_method(number, initial_number_system)
-        return get_binary_numsys_method(decimalEncodingValues, final_number_system)
-
-    else:  # Translate to binary number system
-        return get_binary_numsys_method(number, final_number_system)
+    elif initial_number_system != 10 and final_number_system == 10:
+        return _get_decimal(number, initial_number_system)
+    elif initial_number_system != 10 and final_number_system != 10:
+        return _get_binary(get_decimal(number, initial_number_system), final_number_system)
+    else:
+        return _get_binary(number, final_number_system)
